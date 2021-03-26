ad5368a446947d8898e092b669dda02a0d97fabe
Ronald Arias <ronald.arias@thoughtworks.com>
2021-03-26 17:49:03 -0500

    Displays the total with reactivity using dollar sign

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,16 @@
 	import { setContext } from "svelte";
 	import Navbar from "./Navbar.svelte";
 	import Button from "./Button.svelte";
+	import Totals from "./Totals.svelte";
 	import ExpenseList from "./ExpenseList.svelte";
 
 	import expensesData from "./expenses";
 
 	let expenses = [...expensesData];
+	
+	$: total = expenses.reduce((accumulator, currentExpense) => {
+		return accumulator + currentExpense.amount;
+	}, 0);
 
 	function removeExpense(id) {
 		expenses = expenses.filter(item => item.id !== id);
@@ -21,6 +26,7 @@
 
 <Navbar />
 <main class="content">
+	<Totals total={total.toFixed(2)} />
 	<ExpenseList {expenses} />
 	{#if expenses.length > 0}
 		<Button title="Clear Expenses" onClick={clearExpenses} />
---
+++ src/Totals.svelte
@@ -0,0 +1,9 @@
+<script>
+    import TotalsTitle from "./Title.svelte";
+
+    export let total = 0;
+</script>
+
+<section>
+    <TotalsTitle title={`Total expenses: $${total}`} />
+</section>
