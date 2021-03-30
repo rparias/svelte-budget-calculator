eaa0923f3be4c380ddebce31d51b0d965d5382fc
Ronald Arias <ronald.arias@thoughtworks.com>
2021-03-29 21:06:05 -0500

    Uses two way binding with bind:value

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -1,6 +1,8 @@
 <script>
     import Title from './Title.svelte';
     import Button from './Button.svelte';
+    let name = '';
+    let amount = null;
 </script>
 
 <section class="form">
@@ -8,11 +10,11 @@
     <form class="expense-form">
         <div class="form-control">
             <label for="name">Name</label>
-            <input id="name" type="text">
+            <input id="name" type="text" bind:value={name}>
         </div>
         <div class="form-control">
             <label for="amount">Amount</label>
-            <input id="amount" type="number">
+            <input id="amount" type="number" bind:value={amount}>
         </div>
         <p class="form-empty">Please fill out all form fields</p>
         <Button title="Add Expense" styleClass="btn btn-block" typeButton="submit" />
