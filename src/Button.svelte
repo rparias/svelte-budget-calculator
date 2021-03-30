42c322eb3b1cf25331b7a4324a37d968d33df16f
Ronald Arias <ronald.arias@thoughtworks.com>
2021-03-29 22:14:00 -0500

    Disables button if the form is empty

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,14 +1,21 @@
 <script>
     export let title = "default title";
-    export let styleClass = "btn btn-primary btn-block"
+    export let styleClass = "btn btn-primary btn-block";
     export let typeButton = "button";
+    export let isDisabled = false;
     export let onClick;
     export let iconClass;
 </script>
 
-<button type={typeButton} class={styleClass} on:click={onClick}>
+<button
+    type={typeButton}
+    class={styleClass}
+    on:click={onClick}
+    class:disabled={isDisabled}
+    disabled={isDisabled}
+>
     {#if iconClass}
-        <i class={iconClass}></i>
+        <i class={iconClass} />
     {/if}
     {title}
 </button>
--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -3,6 +3,8 @@
     import Button from './Button.svelte';
     let name = '';
     let amount = null;
+
+    $: isEmpty = !name || !amount;
 </script>
 
 <section class="form">
@@ -16,8 +18,10 @@
             <label for="amount">Amount</label>
             <input id="amount" type="number" bind:value={amount}>
         </div>
-        <p class="form-empty">Please fill out all form fields</p>
-        <Button title="Add Expense" styleClass="btn btn-block" typeButton="submit" />
+        {#if isEmpty}
+            <p class="form-empty">Please fill out all form fields</p>
+        {/if}
+        <Button title="Add Expense" styleClass="btn btn-block" typeButton="submit" isDisabled={isEmpty}/>
         <Button title="Close" styleClass="close-btn" iconClass="fas fa-times" />
     </form>
 </section>
